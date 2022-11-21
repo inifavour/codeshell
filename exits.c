4ebf9cab1bdb4e325b9431e5ab6b68bccf0273e5
zinando <belovedsamex@yahoo.com>
2022-11-20 17:56:41 -0800

    Fix some bugs

--- exits.c
+++ exits.c
@@ -11,8 +11,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
 	char *s = dest;
-	i = 0;
 
+	i = 0;
 	while (src[i] != '\0' && i < n - 1)
 	{
 		dest[i] = src[i];
@@ -23,8 +23,8 @@ char *_strncpy(char *dest, char *src, int n)
 		j = i;
 		while (j < n)
 		{
-	dest[j] = '\0';
-	j++;
+			dest[j] = '\0';
+			j++;
 		}
 	}
 	return (s);
@@ -33,6 +33,7 @@ char *_strncpy(char *dest, char *src, int n)
 /**
  * *_strncat - concatenates two strings
  * @dest: the first string
+ * @src: the second string
  * @n: the amount of bytes to be maximally used
  * Return: the concatenated string
  */
